bd96d0fe3dae3ca74b56cf3a91c0ea3a34da0ee9
Gazzy21 <nicholas221@icloud.com>
2024-10-28 14:58:51 -0400

    started js

---
+++ script.js
@@ -0,0 +1,8 @@
+// https://api.npoint.io/cdf650906e66cebd400c
+function addClass = new Class {
+    $(#submitDay).on("click", function() {
+        if $(#dayInput === A) {
+            console.log("A");
+        }
+    })
+}
