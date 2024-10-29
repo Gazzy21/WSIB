1787c173949e0a998c746acfebfb89089c4730ef
Gazzy21 <nicholas221@icloud.com>
2024-10-29 11:48:56 -0400

    button works i guess

--- script.js
+++ script.js
@@ -1,8 +1,42 @@
-// https://api.npoint.io/cdf650906e66cebd400c
-function addClass = new Class {
-    $(#submitDay).on("click", function() {
-        if $(#dayInput === A) {
-            console.log("A");
+$(document).ready(function () {
+    const scheduleUrl = "https://api.npoint.io/cdf650906e66cebd400c";
+
+    const bellSchedule = {
+        1: { start: "8:24 AM", end: "9:31 AM" },
+        2: { start: "9:36 AM", end: "10:43 AM" },
+        3: { start: "10:48 AM", end: "11:55 AM" },
+        4: { start: "12:41 PM", end: "1:48 PM" },
+        5: { start: "1:53 PM", end: "3:00 PM" },
+    };
+
+    // $("#submitDay").on("click", function () {
+    //     if ($("#dayInput").val().toUpperCase() === "A") {
+    //         console.log("A");
+
+    //     } else if ($("#dayInput").val().toUpperCase() === "B") {
+    //         console.log("B");
+    //     } else if ($("#dayInput").val().toUpperCase() === "C") {
+    //         console.log("C");
+    //     } else if ($("#dayInput").val().toUpperCase() === "D") {
+    //         console.log("D");
+    //     } else if ($("#dayInput").val().toUpperCase() === "E") {
+    //         console.log("E");
+    //     } else if ($("#dayInput").val().toUpperCase() === "F") {
+    //         console.log("F");
+    //     } else if ($("#dayInput").val().toUpperCase() === "G") {
+    //         console.log("G");
+    //     } else {
+    //         console.log("Error");
+    //     }
+    // });
+
+    $("#submitDay").on("click", function () {
+        const selectedDay = $("#dayInput").val().toUpperCase();
+        if (!["A", "B", "C", "D", "E", "F", "G"].includes(selectedDay)) {
+            alert("Please enter a valid day (A-G)");
+            return;
         }
-    })
-}
+    });
+
+    
+});
